8bf61ec1a222f700a682f50bf375bf9b5bf93e07
Ganyushin, Dmitry <ganyushindi@ornl.gov>
2024-07-03 14:07:51 -0400

    Adjusted test

--- tests/unit/bm3dornl/test_bm3d_ring_artifact_removal_ms.py
+++ tests/unit/bm3dornl/test_bm3d_ring_artifact_removal_ms.py
@@ -4,9 +4,35 @@ import numpy as np
 from bm3dornl.bm3d import bm3d_ring_artifact_removal_ms
 
 
+size_x = 256
+size_y = 256
+
 @pytest.fixture
 def setup_sinogram():
-    return np.random.rand(256, 256)
+    return np.random.rand(size_x, size_y)
+
+
+def test_bm3d_ring_artifact_removal_ms(
+    setup_sinogram,
+):
+    sinogram = setup_sinogram
+
+    result = bm3d_ring_artifact_removal_ms(sinogram, k=4)
+
+    assert result is not None
+
+    r, c = result.shape
+
+    assert c == size_x
+    assert r == size_y
+
+    result_single_pass = bm3d_ring_artifact_removal_ms(sinogram, k=0)
+    assert result_single_pass is not None
+
+    r, c = result_single_pass.shape
+
+    assert c == size_x
+    assert r == size_y
 
 
 @pytest.mark.skip(reason="Change function signature")
